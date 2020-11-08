c6aa5f4bcac0aed54d35bc4bf7b90db7f249becc
Joseph Nagy <jn135@duke.edu>
2020-11-08 15:41:22 -0500

    implimented get_timestamp

--- data-collector.py
+++ data-collector.py
@@ -1,4 +1,15 @@
 import mysql.connector
+import datetime
+import os
+
+
+def get_timestamp(path):
+    t = os.path.getmtime(path)
+    if(t):
+        return datetime.datetime.fromtimestamp(t)
+    else:
+        print("No timestamp available")
+        return None
 
 #method: insert sighting into sighting table
 def insert_sighting(cursor, cnx, turtle_tag_id, latitude, longitude, drone_image, timestamp): #no sighting_id bc AI field
@@ -12,10 +23,14 @@ def insert_sighting(cursor, cnx, turtle_tag_id, latitude, longitude, drone_image
 
 if __name__ == '__main__':
     #make database connection
-    cnx = mysql.connector.connect(
-        host = "dct-turtle-uav.cojdookdlkis.us-east-1.rds.amazonaws.com", #AWS RDB endpoint
-        user = "admin",
-        password = "DukeConservationTech1!",
-        database = "dct_turtle_uav_db"
-    )
-    cursor=cnx.cursor() #create db cursor
+    #cnx = mysql.connector.connect(
+        #host = "dct-turtle-uav.cojdookdlkis.us-east-1.rds.amazonaws.com", #AWS RDB endpoint
+        #user = "admin",
+        #password = "DukeConservationTech1!",
+        #database = "dct_turtle_uav_db"
+    #)
+    #cursor=cnx.cursor() #create db cursor
+
+    #test date getter method
+    path = "/Users/josephnagy/Desktop/test.jpg"
+    print(get_timestamp(path))
