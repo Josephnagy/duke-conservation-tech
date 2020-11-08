4a0ce19148beaacaf0e51ad580b1b9a22d1c2a45
Joseph Nagy <jn135@duke.edu>
2020-11-08 14:00:52 -0500

    first insert

--- data-collector.py
+++ data-collector.py
@@ -1,10 +1,17 @@
-import mysql.connector as sql
+import mysql.connector
 
 if __name__ == '__main__':
-    database = sql.connect(
+    #make database connection
+    cnx = mysql.connector.connect(
         host = "dct-turtle-uav.cojdookdlkis.us-east-1.rds.amazonaws.com", #AWS RDB endpoint
         user = "admin",
-        password = "DukeConservationTech1!"
+        password = "DukeConservationTech1!",
+        database = "dct_turtle_uav_db"
     )
-    mycursor=database.cursor()
-    print(database)
+    #create db cursor
+    cursor=cnx.cursor()
+    #insert_stmt = ("INSERT INTO sighting (turtle_tag_id) VALUES (%s) ")
+    #data = (1,)
+    #cursor.execute(insert_stmt, data)
+    #cnx.commit()
+    print(cnx.database)
