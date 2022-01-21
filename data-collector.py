7c9eb52d507d79d1b53c36617bc3d2939caa7c61
Joseph Nagy <nagyjoseph9@gmail.com>
2022-01-20 21:37:31 -0500

    removed sensitive information

--- data-collector.py
+++ data-collector.py
@@ -2,16 +2,27 @@ import mysql.connector
 import datetime
 import os
 import cv2
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+import exifread as ef
 
 #insert sighting into sighting table
 def insert_sighting(cursor, cnx, turtle_tag_id, latitude, longitude, drone_image, timestamp): #no sighting_id bc AI field
     insert_stmt = ("""INSERT INTO sighting (turtle_tag_id, latitude, longitude, drone_image, timestamp)
                     VALUES (%s,%s,%s,%s,%s)""") #parameters are always %s
     data = (turtle_tag_id, latitude, longitude, drone_image, timestamp)
-
     #update db with new data
     cursor.execute(insert_stmt, data)
     cnx.commit()
+    return
+
+def get_exif(filename):
+    exif = Image.open(filename)._getexif()
+    if exif is not None:
+        for key, value in exif.items():
+            name = TAGS.get(key, key)
+            exif[name] = exif.pop(key)
+    print(exif.keys())
 
 def get_timestamp(path):
     t = os.path.getmtime(path)
@@ -35,9 +46,12 @@ def connect_to_db(host, user, pw, db):
 if __name__ == '__main__':
     #DB credentials
     host = "dct-turtle-uav.cojdookdlkis.us-east-1.rds.amazonaws.com" #AWS RDB endpoint
-    user = "admin"
-    password = "DukeConservationTech1!"
+    # user = *insert user* 
+    # password = *insert password*
     database = "dct_turtle_uav_db"
 
     #establish DB connection
     #cnx = connect_to_db(host, user, password, database)
+
+    #test get exif
+    path = "/Users/josephnagy/Desktop/test.jpg"
